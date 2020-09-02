cd4439410e7659b3956baeaca9563629a01d5ffe
tommaclean <tom.j.maclean@gmail.com>
2020-09-02 17:44:30 -0400

    added logo

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../../Logo/Logo'
 
 import classes from './Toolbar.module.css'
 
@@ -6,7 +7,7 @@ import classes from './Toolbar.module.css'
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
