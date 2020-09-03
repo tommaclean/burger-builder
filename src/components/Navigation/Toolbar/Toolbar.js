692467aa854925d36d0f4f1bface45fa520e422a
tommaclean <tom.j.maclean@gmail.com>
2020-09-03 12:14:43 -0400

    added menu button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 import classes from './Toolbar.module.css'
 
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div onClick={props.menuClicked}>MENU</div>
+        <DrawerToggle menuClicked={props.menuClicked}/>
         <div className={classes.Logo}>
             <Logo />
         </div>
