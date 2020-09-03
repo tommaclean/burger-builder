2bb2b73ac2c1720527462780c9b0e5ff2d143756
tommaclean <tom.j.maclean@gmail.com>
2020-09-02 21:27:08 -0400

    adjusted sidedrawer for mobile

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,9 @@ import classes from './Toolbar.module.css'
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
         <nav>
             <NavigationItems />
         </nav>
