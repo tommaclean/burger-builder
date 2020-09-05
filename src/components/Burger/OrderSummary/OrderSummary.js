801d2fa69560837662013e34ad2141c0233f160c
tommaclean <tom.j.maclean@gmail.com>
2020-09-05 14:48:51 -0400

    converted modal to class

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,8 +1,11 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
 class OrderSummary extends Component  {
+    componentDidUpdate() {
+        console.log('Order Summary DidUpdate')
+    }
     render () {
         const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
             return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
