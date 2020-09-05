c93003af87704cb1504c15194e8053b66e52d2c9
tommaclean <tom.j.maclean@gmail.com>
2020-09-05 14:56:21 -0400

    optimized OrderSummary rendering

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,6 +5,13 @@ import Backdrop from '../Backdrop/Backdrop'
 import classes from './Modal.module.css'
 
 class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate () {
+        console.log('Modal WillUpdate')
+    }
     render () {
         return (
             <Aux>
